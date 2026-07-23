18f9efbed7caee9e7295961e61aa8aedd4924125
lancubal <lunalancuba.sistemas@gmail.com>
2026-07-23 14:49:44 -0300

    Garbage Collection

--- clox/vm.h
+++ clox/vm.h
@@ -4,6 +4,7 @@
 #include "object.h"
 #include "table.h"
 #include "value.h"
+#include <stddef.h>
 
 #define FRAMES_MAX 64
 #define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
@@ -22,7 +23,12 @@ typedef struct {
   Table globals;
   Table strings;
   ObjUpvalue *openUpvalues;
+  size_t bytesAllocated;
+  size_t nextGC;
   Obj *objects;
+  int grayCount;
+  int grayCapacity;
+  Obj **grayStack;
 } VM;
 
 typedef enum {
