6f9f25a28fa6d6cb18c714036d97eaaa09a48b34
lancubal <lunalancuba.sistemas@gmail.com>
2026-08-04 13:09:13 -0300

    Superclasses

--- clox/chunk.h
+++ clox/chunk.h
@@ -7,6 +7,7 @@
 typedef enum {
   OP_RETURN,
   OP_CLASS,
+  OP_INHERIT,
   OP_METHOD,
   OP_CONSTANT,
   OP_NIL,
@@ -22,6 +23,7 @@ typedef enum {
   OP_SET_UPVALUE,
   OP_GET_PROPERTY,
   OP_SET_PROPERTY,
+  OP_GET_SUPER,
   OP_EQUAL,
   OP_GREATER,
   OP_LESS,
@@ -32,6 +34,7 @@ typedef enum {
   OP_LOOP,
   OP_CALL,
   OP_INVOKE,
+  OP_SUPER_INVOKE,
   OP_CLOSURE,
   OP_CLOSE_UPVALUE,
   OP_ADD,
